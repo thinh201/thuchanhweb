2c4ab72d144a9b7205aebbe25ff5b2a22abf666a
Đào Quang Thịnh <215748020110076@vinhuni.edu.vn>
2023-11-16 11:53:31 +0700

    Add project files.

---
+++ startup/Components/MenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using startup.Models;
+
+namespace startup.Components
+{
+    [ViewComponent(Name = "MenuView")]
+    public class MenuViewComponent : ViewComponent
+    {
+        private DataContext _context;
+        public MenuViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var listofMenu = (from m in _context.Menus
+                              where (m.IsActive == true) && (m.Position == 1)
+                              select m).ToList();
+            //m.Position == 1 là những menu nằm phía trên
+            //m.Position == 2 là những menu nằm phía dưới
+            return await Task.FromResult((IViewComponentResult)View("Default", listofMenu));
+        }
+    }
+}
